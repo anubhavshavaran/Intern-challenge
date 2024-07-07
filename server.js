018c491f14ec7f2de0e609333109de6bad3fc777
Anubhav Shavaran <anubhavshavaran5678@gmail.com>
2024-07-07 23:46:06 +0530

    # 1 - Initial Comit - Set all the files.

---
+++ server.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require("mongoose");
+const dotenv = require('dotenv');
+const app = require("./app");
+
+dotenv.config();
+
+// Connecting to ther database.
+const DB = process.env.DATABASE_LOCAL;
+const DB_NAME = process.env.DATABASE_NAME;
+mongoose.connect(DB, {dbName: DB_NAME}).then(function () {
+    console.log('Database has been successfully connected...');
+});
+
+// Listening to the app.
+const PORT = process.env.PORT
+app.listen(PORT, function () {
+    console.log(`App is running at PORT:${PORT}...`);
+});
